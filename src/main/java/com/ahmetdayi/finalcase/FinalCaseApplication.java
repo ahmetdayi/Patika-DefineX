93ca811fd77505bc8e98b4648873549147fa031b
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-06 23:54:12 +0300

    project created

---
+++ src/main/java/com/ahmetdayi/finalcase/FinalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetdayi.finalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCaseApplication.class, args);
+	}
+
+}
